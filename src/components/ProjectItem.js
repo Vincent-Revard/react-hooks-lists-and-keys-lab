bbc1971915daf0bc363e6251b72393c8eefaebe6
Vincent Revard <Vincent.Revard@gmail.com>
2024-02-19 00:24:40 -0800

    Complete Lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologyMapped = technologies.map(technology => (
+    <span key={technology}>{technology}</span>
+  ))
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      {technologyMapped}
       </div>
     </div>
   );
